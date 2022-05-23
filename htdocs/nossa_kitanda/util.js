16a11920c9e8cd55c2cca820fa3df882e86f94fe
Luca Gulak <lc.gk@hotmail.com>
2022-05-23 18:37:17 -0300

    Adiciona logica de adicionar e remover produtos da cesta

--- htdocs/nossa_kitanda/util.js
+++ htdocs/nossa_kitanda/util.js
@@ -1,3 +1,10 @@
+const removeAllChilds = (parent) => {
+    while (parent.lastChild) {
+        parent.removeChild(parent.lastChild)
+    }
+};
+
+let idCounter = 0;
 class Product {
     constructor(productName, productPrice) {
         this.productName = productName;
@@ -7,6 +14,9 @@ class Product {
                 style: "currency",
             }
         ).format(productPrice);
+        this.productPriceRaw = productPrice;
+
+        this.productId = idCounter++; // Id único para cada produto criado...
     }
 }
 
@@ -26,10 +36,85 @@ function populateProducts() {
 
     products.forEach(product => {
         let li = document.createElement("li");
+        li.setAttribute('data-productid', product.productId);
         li.innerHTML = `<span>${product.productName}</span><span>${product.productPrice}</span>`
+
+        li.addEventListener(
+            "click", handleAddProductToCart
+        );
 
         productsList.appendChild(li);
     });
+}
+
+function handleAddProductToCart(e) {
+    const productId = e.target.getAttribute('data-productid');
+    if (productId == undefined)
+        return;
+
+    const product = products[productId];
+
+    let onList = false;
+
+    cart.forEach(product => {
+        if (product.productId == productId) {
+            alert(`O produto ${product.productName} já foi adicionado a sua cesta!`);
+            onList = true;
+        }
+    });
+    
+    if (onList)
+        return;
+
+    cart = [...cart, product]; // Adiciona o produto selecionado ao final da cesta
+
+    updateCart(); // Atualiza a cesta de produtos
+}
+
+function updateCart() {
+    const cartList = document.querySelector("#cart");
+    removeAllChilds(cartList);
+
+    let totalPrice = 0;
+
+    cart.forEach(product => {
+        let li = document.createElement("li");
+        li.setAttribute('data-productid', product.productId);
+        li.innerHTML = `<span>${product.productName}</span><span>${product.productPrice}</span>`
+
+        li.addEventListener(
+            "click", handleRemoveProductFromCart
+        );
+
+        cartList.appendChild(li);
+
+        // Calcula o valor total dos produtos
+        totalPrice += product.productPriceRaw;
+    });
+
+    // Formata o preço
+    let formattedPrice = new Intl.NumberFormat(
+        "pt-BR", {
+            currency: "BRL",
+            style: "currency",
+        }
+    ).format(totalPrice);
+
+    // Altera o valor total no header e na cesta
+    document.querySelector("#total-header").innerHTML = `TOTAL: ${formattedPrice}`;
+    document.querySelector("#total-price").innerHTML = `TOTAL: ${formattedPrice}`;
+}
+
+function handleRemoveProductFromCart(e) {
+    const productId = e.target.getAttribute('data-productid');
+    if (productId == undefined)
+        return;
+
+    cart = cart.filter((product) => {
+        return product.productId != productId
+    })
+
+    updateCart(); // Atualiza a cesta de produtos
 }
 
 export { populateProducts };
